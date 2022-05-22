a3ca32da536db5225d20de301277ded20a0c6f6b
Muhammed Shittu <coolzyte@gmail.com>
2022-05-22 21:05:22 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
